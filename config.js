0573c776b8a0b718fbdba0d3927af76d8c2b01ea
mdshahrouq <mdshah1994@gmail.com>
2017-02-19 20:00:27 +0530

    Added my cv pages

---
+++ config.js
@@ -0,0 +1,1 @@
+IDRViewer.config = {"pagecount":4,"title":"","author":"","fileName":"MD_ShahrouqSProfile.pdf","bounds":[[935,1210],[935,1210],[935,1210],[935,1210]],"bookmarks":[],"thumbnailType":"jpg","pageType":"html","pageLabels":["1","2","3","4"]};
